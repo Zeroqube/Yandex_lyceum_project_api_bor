49acb43efb59388ec84f87c9c6ce63aab4e1dbf0
Zeroqube <maxbystrov2006@yandex.ru>
2021-04-24 18:13:20 +0300

    structure added

--- main.py
+++ main.py
@@ -1,44 +1,106 @@
-from telegram.ext import Updater, MessageHandler, Filters
-from telegram.ext import CallbackContext, CommandHandler
-from telegram.ext import CommandHandler
-from telegram import ReplyKeyboardMarkup
-import random
-import math
+import asyncio
+import sqlite3
+from discord.ext import commands
+import datetime as dt
+# в data храниться только токен для удобного использования и сохранения секретности токена
 from .data import TOKEN
-
-home_keyboard = [['/dice', '/timer']]
-dice_keyboard = [['/6', '/2x6'], ['/20', '/back']]
-timer_keyboard = [['/1m'], ['/5m'], ['/3s', '/back']]
-home_markup = ReplyKeyboardMarkup(home_keyboard, one_time_keyboard=False)
-dice_markup = ReplyKeyboardMarkup(home_keyboard, one_time_keyboard=False)
-timer_markup = ReplyKeyboardMarkup(home_keyboard, one_time_keyboard=False)
+SQL_FILE = ''
 
 
-def start(update, context):
-    update.message.reply_text(
-        "Привет! Я бот помощник для игр!")
-    update.message.reply_text(
-        "Засечь время, или бросить кубики?", reply_markup=home_markup)
+class SqlAdapter:
+    def __init__(self, sql_file):
+        self.sql_file = sql_file
+        self.con = sqlite3.connect(sql_file)
+
+    async def take_object(self):
+        pass
+
+    async def take_objects(self):
+        pass
+
+    async def create_object(self):
+        pass
+
+    async def delete_object(self):
+        pass
+
+    async def change_object(self):
+        pass
+
+    async def create_user(self):
+        pass
+
+    async def take_unused_id(self):
+        pass
 
 
-def help(update, context):
-    update.message.reply_text(
-        "Засеку ремя и брошу кубики за вас")
+class ToDo(commands.Cog):
+    def __init__(self, bot, adapter):
+        self.bot = bot
+        self.adapter = adapter
+
+    @commands.command(name='help')
+    async def set_problem(self, ctx, obj):
+        pass
+
+    @commands.command(name='set problem with deadline')
+    async def set_problem(self, ctx, name, date):
+        pass
+
+    @commands.command(name='delete problem with deadline')
+    async def delete_problem(self, ctx, name):
+        pass
+
+    @commands.command(name='change problem with deadline')
+    async def delete_problem(self, ctx, name, field, value):
+        pass
+
+    @commands.command(name='set problem')
+    async def set_problem(self, ctx, name):
+        pass
+
+    @commands.command(name='delete problem')
+    async def delete_problem(self, ctx, name):
+        pass
+
+    @commands.command(name='change problem')
+    async def delete_problem(self, ctx, name, field, value):
+        pass
+
+    @commands.command(name='set event')
+    async def set_event(self, ctx, name, date):
+        pass
+
+    @commands.command(name='delete event')
+    async def delete_event(self, ctx, name):
+        pass
+
+    @commands.command(name='change event')
+    async def delete_problem(self, ctx, name, field, value):
+        pass
+
+    @commands.command(name='add description')
+    async def delete_event(self, ctx, name, description):
+        pass
+
+    @commands.command(name='change description')
+    async def delete_event(self, ctx, name, description):
+        pass
+
+    @commands.command(name='delete description')
+    async def delete_event(self, ctx, name):
+        pass
+
+    @commands.command(name='get all')
+    async def delete_event(self, ctx, typ):
+        pass
+
+    @commands.command(name='get all')
+    async def delete_event(self, ctx, date):
+        pass
 
 
-def main():
-    updater = Updater(TOKEN, use_context=True)
-
-    # Получаем из него диспетчер сообщений.
-    dp = updater.dispatcher
-
-    # Регистрируем обработчик в диспетчере.
-    dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("help", help))
-
-    updater.start_polling()
-    updater.idle()
-
-
-if __name__ == '__main__':
-    main()
+sql_adapter = SqlAdapter(SQL_FILE)
+bot = commands.Bot(command_prefix='!!')
+bot.add_cog(ToDo(bot, sql_adapter))
+bot.run(TOKEN)
