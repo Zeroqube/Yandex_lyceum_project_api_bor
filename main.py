25cd22c2a4b9c438469cb88dac54aceb5adf11e6
Zeroqube <maxbystrov2006@yandex.ru>
2021-04-26 22:00:48 +0300

    New func

--- main.py
+++ main.py
@@ -31,8 +31,8 @@ async def on_ready():
 
 @bot.command(name='set_task')
 async def set_task(ctx, name):
-    if len(name) > 10:
-        await ctx.send('Название задачи не должно превышать 10 символов!')
+    if len(name) > 100:
+        await ctx.send('Название задачи не должно превышать 100 символов!')
         return
     channel_id = ctx.author.id
     if not cur.execute('SELECT id FROM channels WHERE id = ?', (channel_id, )).fetchone():
@@ -42,7 +42,27 @@ async def set_task(ctx, name):
         return
     cur.execute('INSERT INTO todo(channel_id, task_name) VALUES (?, ?)', (channel_id, name, ))
     con.commit()
+    await ctx.message.add_reaction('👍')
     print(channel_id, name)
+
+
+@bot.command(name='set_tasks')
+async def set_tasks(ctx, *names):
+    channel_id = ctx.author.id
+    if not cur.execute('SELECT id FROM channels WHERE id = ?', (channel_id,)).fetchone():
+        cur.execute('INSERT INTO channels VALUES (?)', (channel_id,))
+    con.commit()
+    for name in names:
+        if len(name) > 100:
+            await ctx.send('Название задачи не должно превышать 100 символов!')
+            return
+        if cur.execute('SELECT task_name FROM todo WHERE channel_id = ? AND task_name = ?', (channel_id, name)).fetchone():
+            await ctx.send(f'Задача с именнем {name} уже есть!')
+            return
+        cur.execute('INSERT INTO todo(channel_id, task_name) VALUES (?, ?)', (channel_id, name, ))
+        await ctx.message.add_reaction('👍')
+        print(channel_id, name)
+    con.commit()
 
 
 @bot.command(name='set_date')
@@ -61,6 +81,7 @@ async def set_date(ctx, name, date):
     SET date = ?
     WHERE channel_id = ? AND task_name = ?''', (date, channel_id, name))
     con.commit()
+    await ctx.message.add_reaction('👍')
 
 
 @bot.command(name='set_time')
@@ -81,6 +102,7 @@ async def set_time(ctx, name, time):
     SET time = ?
     WHERE channel_id = ? AND task_name = ?''', (time, channel_id, name))
     con.commit()
+    await ctx.message.add_reaction('👍')
 
 
 @bot.command(name='set_description')
@@ -98,6 +120,7 @@ async def set_description(ctx, name, *description):
     SET description = ?
     WHERE channel_id = ? AND task_name = ?''', (description, channel_id, name))
     con.commit()
+    await ctx.message.add_reaction('👍')
 
 
 @bot.command(name='get_description')
@@ -111,7 +134,7 @@ async def get_description(ctx, name):
         await ctx.send(f'Задачи с именнем {name} ещё нет!')
         return
     res = cur.execute('SELECT date, time, description FROM todo WHERE channel_id = ?', (channel_id, )).fetchone()
-    s = f'Вы описали задачу {name} {res[0]} {res[1]} так:\n' + res[2]
+    s = f'Вы описали задачу {name} {res[0]} {res[1]} так:\n' + str(res[2])
     await ctx.send(s)
 
 
@@ -125,6 +148,26 @@ async def get_tasks(ctx):
         await ctx.send('У вас нет сейчас задач')
         return
     res = cur.execute('SELECT task_name, date, time FROM todo WHERE channel_id = ?', (channel_id, )).fetchall()
+    s = 'Ваши задачи:\n' + '\n'.join(normalize(res))
+    await ctx.send(s)
+
+
+@bot.command(name='get_with_date')
+async def get_with_date(ctx, date):
+    channel_id = ctx.author.id
+    if not cur.execute('SELECT id FROM channels WHERE id = ?', (channel_id, )).fetchone():
+        await ctx.send('У вас ещё не было задач')
+        return
+    if not cur.execute('SELECT task_name, date, time FROM todo WHERE channel_id = ?', (channel_id, )).fetchone():
+        await ctx.send('У вас нет сейчас задач')
+        return
+    if date == 'None':
+        date = 'Nulle'
+    else:
+        day, month, year = map(int, date.split('.'))
+        date = dt.date(year, month, day)
+    res = cur.execute('SELECT task_name, date, time FROM todo WHERE channel_id = ? AND date = ?',
+                      (channel_id, date, )).fetchall()
     s = 'Ваши задачи:\n' + '\n'.join(normalize(res))
     await ctx.send(s)
 
